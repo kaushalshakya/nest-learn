8a6cc2023c4b764dd95e25725a247981a21dfaee
kaushalshakya <kshakya101@gmail.com>
2024-07-12 22:28:27 +0545

    first route

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
 import { AppService } from '../services/app.service';
+import { Response } from 'express';
+import { APIResponse } from 'src/interfaces/apiResponse.interface';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Res() res: Response): Response<APIResponse> {
+    const response = this.appService.getHello();
+    return res
+      .status(HttpStatus.OK)
+      .json({ status: 200, message: response, asdasd: 'asdass' });
   }
 }
